b16d5b198c4c254dda4319b6637324323ae4f9ac
AddiS4 <addi.kumar@knoldus.in>
2019-10-29 17:27:12 +0530

    HOTFIX-FormatingCode | Added mocking and formatted

--- inventory/src/main/scala/com/knoldus/inventoryservice/util/JSONSupport.scala
+++ inventory/src/main/scala/com/knoldus/inventoryservice/util/JSONSupport.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.knoldus.data.model.Inventory
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
-trait JSONParser extends SprayJsonSupport with DefaultJsonProtocol {
+trait JSONSupport extends SprayJsonSupport with DefaultJsonProtocol {
 
   implicit val itemFormat: RootJsonFormat[Inventory] = jsonFormat8(Inventory)
 
